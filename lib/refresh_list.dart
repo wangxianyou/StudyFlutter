5a03afa16301cc1b6cb4d76cd55d42f865f63dd2
wangxianyou <303681285@qq.com>
2020-05-29 10:00:47 +0800

    1.网络请求后数据渲染

--- lib/refresh_list.dart
+++ lib/refresh_list.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RefreshListPage extends StatefulWidget {
   @override
@@ -10,36 +14,62 @@ class RefreshListPage extends StatefulWidget {
 }
 
 class MyState extends State<RefreshListPage> {
-  var items = List.generate(15, (i) => i);
+  var items = new List();
   var _controller = new ScrollController();
   var isRequest = false;
   var isHasMore = true;
+  int pageIndex = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _controller.addListener(() {
       if (_controller.position.pixels == _controller.position.maxScrollExtent) {
-        _getMoreData(items.length, items.length >=30 ? items.length:items
-            .length + 15);
+        _getMoreData();
+      } else if (_controller.position.pixels ==
+          _controller.position.minScrollExtent) {
+        initNetData();
       }
+    });
+
+    initNetData();
+  }
+
+  /**
+   * 获取网络数据
+   */
+  void initNetData() async {
+    await http
+        .get("http://www.wanandroid.com/project/list/1/json?cid=1")
+        .then((http.Response response) {
+      var result = json.decode(response.body);
+      var list = result["data"]["datas"];
+      setState(() {
+        pageIndex = 2;
+        items = list;
+      });
     });
   }
 
   /**
    * 加载下一页数据
    */
-  void _getMoreData(var from, var to) async {
+  void _getMoreData() async {
     if (!isRequest) {
       setState(() {
         isRequest = true;
       });
-      List<int> list = await request(from, to);
-      setState(() {
-        isHasMore = list.isNotEmpty;
-        items.addAll(list);
-        isRequest = false;
+      await http
+          .get("http://www.wanandroid.com/project/list/$pageIndex/json?cid=1")
+          .then((http.Response response) {
+        var result = json.decode(response.body);
+        var list = result["data"]["datas"];
+        setState(() {
+          pageIndex++;
+          isHasMore = list.isNotEmpty;
+          items.addAll(list);
+          isRequest = false;
+        });
       });
     }
   }
@@ -54,19 +84,20 @@ class MyState extends State<RefreshListPage> {
     return Padding(
       padding: EdgeInsets.all(10),
       child: Center(
-        child: isHasMore?Opacity(
-          opacity: isRequest ? 1.0 : 0.0,
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation(Colors.black),
-          ),
-        ):Text("加载全部"),
+        child: isHasMore
+            ? Opacity(
+                opacity: isRequest ? 1.0 : 0.0,
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.black),
+                ),
+              )
+            : Text("加载全部"),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("refresh-list"),
@@ -75,12 +106,14 @@ class MyState extends State<RefreshListPage> {
           itemCount: items.length + 1,
           controller: _controller,
           itemBuilder: (context, index) {
-
             if (index == items.length) {
               return _progress();
             } else {
-              return ListTile(
-                title: Text("条目的：index = $index"),
+              return Card(
+                color: Colors.blue,
+                child: ListTile(
+                  title: Text("条目的：index = ${items[index]["title"]}"),
+                ),
               );
             }
           }),
